186909af061e6272147a397584f4b1fd03e1a706
Wangxt <2601183227@qq.com>
2024-12-26 00:53:20 +0800

    H

--- sys-manage/src/main/java/com/haohaodayouxi/manage/constants/enums/login/LoginLimitEnum.java
+++ sys-manage/src/main/java/com/haohaodayouxi/manage/constants/enums/login/LoginLimitEnum.java
@@ -13,8 +13,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum LoginLimitEnum {
     LOGIN_ERROR_NUM("21001", "登录错误次数", 6),
-    LOGIN_LOCK_TIME("21002", "登录锁定分钟", 30),
-    LOGIN_ERROR_SHOW_CODE("21003", "登录错误显示验证码", 3),
+    LOGIN_ERROR_SHOW_CODE("21002", "登录错误显示验证码", 3),
+    LOGIN_LOCK_TIME("21003", "登录锁定分钟", 30),
     ;
     private final String code;
     private final String name;
