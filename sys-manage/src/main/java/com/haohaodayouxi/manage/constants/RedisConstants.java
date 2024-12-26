f0e03c1f47b50200adf98a1a5ea19c5c3520fa24
Wangxt <2601183227@qq.com>
2024-12-27 00:09:11 +0800

    H

---
+++ sys-manage/src/main/java/com/haohaodayouxi/manage/constants/RedisConstants.java
@@ -0,0 +1,10 @@
+package com.haohaodayouxi.manage.constants;
+
+/**
+ * RedisConstants
+ *
+ * @author TONE
+ * @date 2024/12/26
+ */
+public interface RedisConstants {
+}
---
+++ sys-manage/src/main/java/com/haohaodayouxi/manage/constants/SysConstants.java
@@ -0,0 +1,11 @@
+package com.haohaodayouxi.manage.constants;
+
+/**
+ * SysConstants
+ *
+ * @author TONE
+ * @date 2024/12/26
+ */
+public interface SysConstants {
+    
+}
