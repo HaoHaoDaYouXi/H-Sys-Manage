aa998744d356f41b71054f43e33c3df0675cb2b0
Wangxt <2601183227@qq.com>
2024-12-27 15:40:36 +0800

    H

--- sys-manage/src/main/java/com/haohaodayouxi/manage/constants/SysConstants.java
+++ sys-manage/src/main/java/com/haohaodayouxi/manage/constants/SysConstants.java
@@ -7,5 +7,10 @@ package com.haohaodayouxi.manage.constants;
  * @date 2024/12/26
  */
 public interface SysConstants {
-    
+    /**
+     * 最顶级ID
+     * 目前使用场景：菜单顶级
+     */
+    Long TOP_LEVEL_ID = 0L;
+
 }
