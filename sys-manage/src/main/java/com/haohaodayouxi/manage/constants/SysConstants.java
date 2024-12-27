58e9b21d1011183bc1806e4bff59be4f09e73627
Wangxt <2601183227@qq.com>
2024-12-28 01:04:41 +0800

    H

--- sys-manage/src/main/java/com/haohaodayouxi/manage/constants/SysConstants.java
+++ sys-manage/src/main/java/com/haohaodayouxi/manage/constants/SysConstants.java
@@ -1,5 +1,7 @@
 package com.haohaodayouxi.manage.constants;
 
+import com.haohaodayouxi.common.util.constants.StringConstant;
+
 /**
  * SysConstants
  *
@@ -12,5 +14,20 @@ public interface SysConstants {
      * 目前使用场景：菜单顶级
      */
     Long TOP_LEVEL_ID = 0L;
+
+    /**
+     * 系统名称
+     */
+    String PROJECT_NAME = System.getProperty("spring.application.name", "manage");
+
+    /**
+     * 系统端口
+     */
+    String PROJECT_PORT = System.getProperty("server.port", "10000");
+
+    /**
+     * 项目缓存前缀
+     */
+    String PROJECT_CACHE_PREFIX = PROJECT_NAME + StringConstant.COLON + PROJECT_PORT;
 
 }
