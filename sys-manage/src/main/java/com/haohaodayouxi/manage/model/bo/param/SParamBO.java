a62c1233c8dc8078da1e8436dc3977b5ff2f5837
Wangxt <2601183227@qq.com>
2024-12-29 23:13:48 +0800

    H

---
+++ sys-manage/src/main/java/com/haohaodayouxi/manage/model/bo/param/SParamBO.java
@@ -0,0 +1,49 @@
+package com.haohaodayouxi.manage.model.bo.param;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * ParamBO
+ *
+ * @author TONE
+ * @date 2024/12/29
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SParamBO implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1363111550768463465L;
+
+    /**
+     * 编码
+     */
+    private Long paramCode;
+
+    /**
+     * 参数分类(上级编码，0-第一级)
+     */
+    private Long paramParentCode;
+
+    /**
+     * 参数名称
+     */
+    private String paramName;
+
+    /**
+     * 参数值
+     */
+    private String paramValue;
+
+    /**
+     * 排序code
+     */
+    private Integer paramSortCode;
+}
--- sys-manage/src/main/java/com/haohaodayouxi/manage/service/impl/SParamServiceImpl.java
+++ sys-manage/src/main/java/com/haohaodayouxi/manage/service/impl/SParamServiceImpl.java
@@ -1,6 +1,7 @@
 package com.haohaodayouxi.manage.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.haohaodayouxi.common.core.interfaces.InitService;
 import com.haohaodayouxi.manage.mapper.SParamMapper;
 import com.haohaodayouxi.manage.model.db.SParam;
 import com.haohaodayouxi.manage.service.SParamService;
@@ -15,7 +16,13 @@ import java.util.List;
  * @date 2024/12/8
  */
 @Service
-public class SParamServiceImpl extends ServiceImpl<SParamMapper, SParam> implements SParamService {
+public class SParamServiceImpl extends ServiceImpl<SParamMapper, SParam> implements SParamService, InitService {
+
+    @Override
+    public void init() {
+        // 查询并放入缓存
+        // 根据查询出来的更新本地参数
+    }
 
     @Override
     public int updateBatch(List<SParam> list) {
