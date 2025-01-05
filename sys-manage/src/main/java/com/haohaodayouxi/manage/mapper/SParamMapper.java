f07bbc38e0dfd1ff36e8af691295eaa39f93d318
Wangxt <2601183227@qq.com>
2025-01-06 00:25:43 +0800

    H

--- sys-manage/src/main/java/com/haohaodayouxi/manage/mapper/SParamMapper.java
+++ sys-manage/src/main/java/com/haohaodayouxi/manage/mapper/SParamMapper.java
@@ -1,7 +1,9 @@
 package com.haohaodayouxi.manage.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.haohaodayouxi.manage.model.bo.param.SParamBO;
 import com.haohaodayouxi.manage.model.db.SParam;
+import com.haohaodayouxi.manage.model.req.param.SParamReq;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -19,4 +21,6 @@ public interface SParamMapper extends BaseMapper<SParam> {
     int updateBatchSelective(@Param("list") List<SParam> list);
 
     int batchInsert(@Param("list") List<SParam> list);
+
+    List<SParamBO> getByReq(SParamReq req);
 }
