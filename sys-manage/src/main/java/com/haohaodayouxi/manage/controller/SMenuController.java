fc6bc3d639d1467315444f89a7a50933bb51ad54
Wangxt <2601183227@qq.com>
2025-03-07 00:27:38 +0800

    H

--- sys-manage/src/main/java/com/haohaodayouxi/manage/controller/SMenuController.java
+++ sys-manage/src/main/java/com/haohaodayouxi/manage/controller/SMenuController.java
@@ -3,17 +3,26 @@ package com.haohaodayouxi.manage.controller;
 import com.haohaodayouxi.common.core.enums.OkResponse;
 import com.haohaodayouxi.common.core.interfaces.AddValid;
 import com.haohaodayouxi.common.core.interfaces.UpdValid;
+import com.haohaodayouxi.common.core.model.bo.ListObjectBO;
 import com.haohaodayouxi.common.core.model.res.Response;
+import com.haohaodayouxi.common.core.model.vo.keyValue.LabelValueVO;
+import com.haohaodayouxi.common.util.constants.StringConstant;
+import com.haohaodayouxi.manage.constants.enums.menu.MenuTypeEnum;
+import com.haohaodayouxi.manage.model.bo.param.SParamBO;
 import com.haohaodayouxi.manage.model.req.menu.SMenuAddOrUpdReq;
 import com.haohaodayouxi.manage.model.req.menu.SMenuListReq;
+import com.haohaodayouxi.manage.model.req.param.SParamReq;
 import com.haohaodayouxi.manage.service.SMenuService;
+import com.haohaodayouxi.manage.service.SParamService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * SMenuController
@@ -28,7 +37,24 @@ public class SMenuController {
 
     @Resource
     private SMenuService sMenuService;
+    @Resource
+    private SParamService paramService;
 
+
+    /**
+     * 详情
+     *
+     * @return res
+     */
+    @GetMapping("/getMenuType")
+    public Response<Object> getMenuType() {
+        List<SParamBO> paramBOS = paramService.getByCache(SParamReq.builder().paramCodes(Arrays.stream(MenuTypeEnum.values()).map(m -> m.getCode().toString()).collect(Collectors.joining(StringConstant.EN_COMMA))).build());
+        if (ObjectUtils.isEmpty(paramBOS)) {
+            return OkResponse.OK.toResponse(Arrays.stream(MenuTypeEnum.values()).map(m -> LabelValueVO.builder().label(m.getName()).value(m.getValue()).build()).toList());
+        } else {
+            return OkResponse.OK.toResponse(paramBOS.stream().map(m -> LabelValueVO.builder().label(m.getParamName()).value(m.getParamValue()).build()).toList());
+        }
+    }
 
     /**
      * 列表查询
@@ -52,6 +78,16 @@ public class SMenuController {
     }
 
     /**
+     * 详情
+     *
+     * @return res
+     */
+    @GetMapping("/detail/{id}")
+    public Response<Object> detail(@PathVariable String id) {
+        return OkResponse.UPDATE.toResponse(sMenuService.getById(id));
+    }
+
+    /**
      * 修改
      *
      * @return res
@@ -60,6 +96,17 @@ public class SMenuController {
     public Response<Object> upd(@RequestBody @Validated(UpdValid.class) SMenuAddOrUpdReq req) {
         sMenuService.addOrUpdSMenu(req);
         return OkResponse.UPDATE.toResponse(true);
+    }
+
+    /**
+     * 批量删除
+     *
+     * @return res
+     */
+    @PostMapping("/batchDel")
+    public Response<Object> batchDel(@RequestBody @Validated ListObjectBO<Long> req) {
+        sMenuService.batchDel(req.getList());
+        return OkResponse.DELETE.toResponse(true);
     }
 
 }
