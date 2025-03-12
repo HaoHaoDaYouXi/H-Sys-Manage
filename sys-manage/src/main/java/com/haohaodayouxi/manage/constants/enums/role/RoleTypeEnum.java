cf879bc3b7055a7437ab73e8bb4f5f34b8b7f5df
Wangxt <2601183227@qq.com>
2025-03-13 01:50:23 +0800

    H

--- sys-manage/src/main/java/com/haohaodayouxi/manage/constants/enums/role/RoleTypeEnum.java
+++ sys-manage/src/main/java/com/haohaodayouxi/manage/constants/enums/role/RoleTypeEnum.java
@@ -4,14 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
- * RoleCodeEnum
+ * RoleTypeEnum
  *
  * @author TONE
  * @date 2025/1/1
  */
 @Getter
 @AllArgsConstructor
-public enum RoleCodeEnum {
+public enum RoleTypeEnum {
 
     SYS_ADMIN(11001L, "系统管理员"),
     REGULAR_USER(11002L, "普通用户"),
