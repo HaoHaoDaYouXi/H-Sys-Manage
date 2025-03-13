6e79c986c093f39f730a985502175f8fdd740ddc
Wangxt <2601183227@qq.com>
2025-03-13 14:09:40 +0800

    H

--- sys-manage/src/main/java/com/haohaodayouxi/manage/constants/enums/param/RootParamCodeEnum.java
+++ sys-manage/src/main/java/com/haohaodayouxi/manage/constants/enums/param/RootParamCodeEnum.java
@@ -15,7 +15,7 @@ public enum RootParamCodeEnum {
 
     MENU_TYPE(1L, "菜单类型"),
     MENU_BUTTON_TYPE(2L, "菜单按钮类型"),
-    ROLE_CODE(11L, "角色编码"),
+    ROLE_TYPE(11L, "角色类型"),
     LOGIN_LIMIT(21L, "登录限制控制"),
     ;
     private final Long code;
