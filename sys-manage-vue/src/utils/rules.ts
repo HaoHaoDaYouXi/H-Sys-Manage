9587c2a481631bc2df34954ee53a53d862a77ad6
Wangxt <2601183227@qq.com>
2025-03-18 13:05:28 +0800

    H

--- sys-manage-vue/src/utils/rules.ts
+++ sys-manage-vue/src/utils/rules.ts
@@ -1,6 +1,16 @@
-﻿/** 公共表单校验规则 */
+﻿
+// 下拉框
+const selectValidator = (rule: any, value: any, callback: any) => {
+  if (!value || (Array.isArray(value) && value.length < 1)) {
+    return callback("请选择");
+  }
+  callback();
+}
+
+/** 公共表单校验规则 */
 const commonFormRules = {
-  required: [{ required: true, min: 1, message: "必填", trigger: "blur" }] // 必填
+  required: [{ required: true, min: 1, message: "必填", trigger: "blur" }],
+  select: [{required: true, validator: selectValidator, trigger: 'change'}],
 }
 
 export default commonFormRules
