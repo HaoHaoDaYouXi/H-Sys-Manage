d30f4f19d44a9787e73516e45de4e348d17c5d0c
Wangxt <2601183227@qq.com>
2025-03-26 00:12:16 +0800

    H

---
+++ sys-manage-vue/src/views/sys/role/index.vue
@@ -0,0 +1,219 @@
+<template>
+  <div class="app-container">
+    <el-card v-loading="loading" shadow="never" class="search-wrapper">
+      <el-form ref="searchFormRef" :inline="true" :model="searchData">
+        <el-form-item prop="roleName" label="角色名">
+          <el-input v-model="searchData.roleName" placeholder="请输入角色名" />
+        </el-form-item>
+        <el-form-item prop="roleType" label="角色类型">
+          <el-select v-model="searchData.roleType" placeholder="请选择角色类型" clearable>
+            <el-option
+              v-for="item in roleTypeList"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" :icon="Search" @click="handleSearch">
+            查询
+          </el-button>
+          <el-button :icon="Refresh" @click="resetSearch">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <el-card v-loading="loading" shadow="never">
+      <div class="toolbar-wrapper">
+        <div>
+          <el-button
+            type="primary"
+            :icon="CirclePlus"
+            @click="openItem(undefined)"
+          >
+            新增角色
+          </el-button>
+          <el-button type="danger" :icon="Delete" @click="batchDel">批量删除</el-button>
+        </div>
+      </div>
+      <div class="table-wrapper">
+        <el-table
+          ref="tableDataRef"
+          :data="tableData"
+          row-key="roleId"
+          border>
+          <el-table-column type="selection" width="50" align="center" />
+          <el-table-column prop="roleName" label="用户名" align="center" />
+          <el-table-column prop="roleTypeStr" label="角色" align="center">
+            <template #default="scope">
+              <el-tag type="primary" effect="plain">
+                {{ scope.row.roleTypeStr }}
+              </el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column prop="updateTime" label="更新时间" align="center" />
+          <el-table-column
+            fixed="right"
+            label="操作"
+            width="150"
+            align="center"
+          >
+            <template #default="scope">
+              <el-button
+                type="primary"
+                text
+                bg
+                size="small"
+                @click="openItem(scope.row.roleId)"
+              >
+                修改
+              </el-button>
+              <el-button
+                type="danger"
+                text
+                bg
+                size="small"
+                @click="handleDelete(scope.row)"
+              >
+                删除
+              </el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <div class="pager-wrapper">
+        <el-pagination
+          background
+          :layout="paginationData.layout"
+          :page-sizes="paginationData.pageSizes"
+          :total="paginationData.total"
+          :page-size="paginationData.pageSize"
+          :currentPage="paginationData.currentPage"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+        />
+      </div>
+    </el-card>
+    <Item ref="itemRef" @success="getTableData" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, reactive, ref, watch } from "vue"
+import { type FormInstance, type FormRules, ElMessage, ElMessageBox, ElTable } from "element-plus"
+import { Search, Refresh, CirclePlus, Delete } from "@element-plus/icons-vue"
+import { usePagination } from "@/hooks/usePagination"
+import { cloneDeep } from "lodash-es"
+import { getRoleTypeApi, pageListApi, batchDelApi } from "@/api/sys/role"
+import { LabelValue, ListObjectBO} from "@/api/commonTypes"
+import Item from "./item.vue"
+
+defineOptions({
+  // 命名当前组件
+  name: "RoleManage"
+})
+
+const { paginationData, handleCurrentChange, handleSizeChange } = usePagination()
+
+const loading = ref<boolean>(false)
+
+const searchFormRef = ref<FormInstance | null>(null)
+const searchData = reactive({
+  roleName: "",
+  roleType: undefined
+})
+
+const roleTypeList = ref<LabelValue[]>([])
+const getRoleType = async () => {
+  try {
+    const { data } = await getRoleTypeApi()
+    roleTypeList.value = data
+  } catch (error) {
+    ElMessage.error("获取角色类型失败")
+  }
+}
+
+const tableDataRef = ref<InstanceType<typeof ElTable>>()
+const tableData = ref<any[]>([])
+const getTableData = async () => {
+  loading.value = true
+  pageListApi({
+      pageNum: paginationData.currentPage,
+      pageSize: paginationData.pageSize,
+      roleName: searchData.roleName || "",
+      roleType: searchData.roleType || ""
+    })
+    .then(({ data }) => {
+      paginationData.total = data.total
+      tableData.value = data.list
+    })
+    .catch(() => {
+      tableData.value = []
+    })
+    .finally(() => {
+      loading.value = false
+    })
+}
+
+const handleSearch = () => {
+  paginationData.currentPage === 1
+    ? getTableData()
+    : (paginationData.currentPage = 1)
+}
+const resetSearch = () => {
+  searchFormRef.value?.resetFields()
+  handleSearch()
+}
+
+const itemRef = ref()
+const openItem = (id?: number) => {
+  itemRef.value.open(id)
+}
+
+const handleDelete = (row: any) => {
+  ElMessageBox.confirm(`正在删除角色：${row.roleName}，确认删除？`, "提示", {
+    confirmButtonText: "确定",
+    cancelButtonText: "取消",
+    type: "warning"
+  }).then(() => {
+    batchDelApi({ list: [row.roleId] } as ListObjectBO)
+      .then(() => {
+        ElMessage.success("删除成功")
+        getTableData()
+      })
+  })
+}
+
+const batchDel = async () => {
+  ElMessageBox.confirm("确定要批量删除吗？", "提示", {
+    type: "warning",
+    confirmButtonText: "确定",
+    cancelButtonText: "取消"
+  }).then(async () => {
+    batchDelApi({ list: tableDataRef.value?.getSelectionRows()?.map((m: any) => m.roleId) } as ListObjectBO).then((res) => {
+      ElMessage.success("删除成功")
+      // 刷新列表
+      getTableData()
+    }).catch((error) => {
+      ElMessage.error("删除失败")
+    })
+  })
+}
+
+/** 监听分页参数的变化 */
+watch([() => paginationData.currentPage, () => paginationData.pageSize], getTableData)
+
+const init = async () => {
+  await getRoleType()
+  await getTableData()
+}
+
+onMounted(async () => {
+  await init()
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ sys-manage-vue/src/views/sys/role/item.vue
@@ -0,0 +1,282 @@
+<template>
+  <div>
+    <el-drawer
+      class="base-el-drawer"
+      :title="dialogTitle"
+      v-model="dialogVisible"
+      size="36%"
+      append-to-body>
+      <el-scrollbar class="base-scrollbar">
+        <el-form
+          ref="formRef"
+          :model="form"
+          :rules="detailFormRules"
+          style="width: 90%"
+          v-loading="formLoading">
+          <el-form-item
+            label="角色名称："
+            label-width="120px"
+            prop="roleName"
+            :rules="detailFormRules.roleName">
+            <el-input
+              v-model="form.roleName"
+              maxlength="20"
+              show-word-limit
+              clearable
+              placeholder="请输入角色名称"></el-input>
+          </el-form-item>
+          <el-form-item
+            label="角色类型："
+            label-width="120px"
+            prop="roleType"
+            :rules="detailFormRules.select">
+            <el-select
+              v-model="form.roleType"
+              placeholder="请选择角色类型"
+              style="width: 100%">
+              <el-option
+                v-for="item in roleTypeList"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item
+            label="角色权限："
+            label-width="120px"
+            prop="roleMenu">
+            <el-card
+              style="width: 100%"
+              shadow="never">
+              <template #header>
+                全选/全不选:
+                <el-switch
+                  v-model="treeNodeAll"
+                  active-text="是"
+                  inactive-text="否"
+                  inline-prompt
+                  @change="handleCheckedTreeNodeAll"
+                />
+                全部展开/折叠:
+                <el-switch
+                  v-model="menuExpand"
+                  active-text="展开"
+                  inactive-text="折叠"
+                  inline-prompt
+                  @change="handleCheckedTreeExpand"
+                />
+              </template>
+              <el-tree
+                class="w-full h-300px !overflow-y-scroll"
+                ref="treeRef"
+                :props="menuTreeProps"
+                lazy
+                :load="lazyLoadMenu"
+                empty-text="加载中，请稍候"
+                node-key="value"
+                show-checkbox
+                check-strictly
+                @check-change="handleCheckedTreeNode"
+              />
+            </el-card>
+          </el-form-item>
+        </el-form>
+      </el-scrollbar>
+      <div class="base-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" :disabled="formLoading" @click="handleSubmit">确 定</el-button>
+      </div>
+    </el-drawer>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, nextTick } from "vue"
+import commonFormRules from "@/utils/rules"
+import { TopId, TrueFalseEnum } from "@/utils/enums"
+import { addApi, detailApi, updApi, getRoleTypeApi } from "@/api/sys/role"
+import { LabelValue } from "@/api/commonTypes"
+import { ElMessage } from "element-plus"
+import { labelValueByParentApi } from "@/api/sys/menu"
+
+const dialogVisible = ref(false)
+const dialogTitle = ref('新增角色')
+
+/** 打开弹窗 */
+const open = async (id?: number) => {
+  resetForm()
+  await getRoleType()
+  dialogVisible.value = true
+  formLoading.value = true
+  dialogTitle.value = (id ? '修改角色' : '新增角色')
+  form.value.roleType = roleTypeList.value[0].value
+  if (id) {
+    try {
+      const detail = await detailApi(id)
+      form.value = detail.data
+      if (detail.data.menuIds) {
+        treeRef.value.setCheckedKeys(detail.data.menuIds)
+      }
+    }catch (e) {
+      ElMessage.error("获取详情失败")
+    }
+  }
+  formLoading.value = false
+}
+/** 提供 open 方法，用于打开弹窗 */
+defineExpose({ open })
+
+const roleTypeList = ref<LabelValue[]>([])
+const getRoleType = async () => {
+  try {
+    const { data } = await getRoleTypeApi()
+    roleTypeList.value = data
+  } catch (error) {
+    ElMessage.error("获取角色类型失败")
+  }
+}
+
+const formLoading = ref(false)
+const formRef = ref()
+const form = ref({
+  roleId: undefined,
+  roleName: '',
+  roleType: '',
+  menuIds: [],
+})
+const detailFormRules = {
+  roleName: [{ required: true, message: '请输入角色名称', trigger: 'blur' }],
+  ...commonFormRules
+}
+
+const treeRef = ref() // 菜单树组件 Ref
+const treeNodeAll = ref(false) // 全选/全不选
+const menuExpand = ref(false) // 展开/折叠
+
+const menuTreeProps = {
+  label: 'label',
+  value: 'value',
+  checkStrictly: true,
+}
+const lazyLoadMenu = async (node: any, resolve: any) => {
+  if (!node.loaded){
+    try {
+      const { data } = await labelValueByParentApi(node.data?.value?node.data.value:TopId)
+      resolve(data)
+    } catch (error) {
+      ElMessage.error("获取下级菜单列表失败")
+      resolve([])
+    }
+  }else {
+    resolve([])
+  }
+}
+
+/** 选/不选 */
+const handleCheckedTreeNode = async (data: any, checked: boolean, hasCheckChild: boolean) => {
+  if (checked && data.hasChildren && !data.children) {
+    await getChild(data)
+  }
+  await nextTick(()=> {
+    treeRef.value.setChecked(data.value, checked, !hasCheckChild)
+    const node=treeRef.value.getNode(data.value)
+    if (checked) {
+      if (node.parent.data.value) {
+        treeRef.value.setChecked(node.parent.data.value, checked, !hasCheckChild)
+      }
+    }else {
+      if (node.data.children) {
+        node.data.children.forEach((child: any) => {
+          treeRef.value.setChecked(child.value, checked, !hasCheckChild)
+        })
+      }
+    }
+  })
+}
+
+/** 全选/全不选 */
+const handleCheckedTreeNodeAll = async () => {
+  if (treeNodeAll.value && !menuExpand.value){
+    menuExpand.value = true
+    await handleCheckedTreeExpand()
+  }
+  for (const node in treeRef.value?.store.nodesMap) {
+    treeRef.value.store.nodesMap[node].checked = treeNodeAll.value
+  }
+}
+
+/** 展开/折叠全部 */
+const handleCheckedTreeExpand = async () => {
+  const nodes = treeRef.value?.store.root.childNodes
+  for (let node in nodes) {
+    if (nodes[node].expanded === menuExpand.value) {
+      continue
+    }
+    if (menuExpand.value) {
+      await getChild(nodes[node].data)
+    }else {
+      nodes[node].expanded = menuExpand.value
+    }
+  }
+}
+
+const getChild = async (data: any) => {
+  const node=treeRef.value.getNode(data.value)
+  if (node.data.hasChildren) {
+    const res = await labelValueByParentApi(data.value)
+    treeRef.value.updateKeyChildren(data.value, res.data)
+    for (const child of res.data) {
+      await getChild(child)
+    }
+  }else {
+    node.isLeaf = true
+  }
+  node.loaded  = true
+  node.expanded = true
+}
+
+const emit = defineEmits(['success'])
+
+const handleSubmit = async () => {
+  formRef.value?.validate((valid: boolean) => {
+    if (valid) {
+      formLoading.value = true
+      const api = form.value.roleId === undefined ? addApi : updApi
+      const tmpForm = {
+        ...form.value,
+        menuIds: [...treeRef.value?.getCheckedKeys()]
+      }
+      api(tmpForm)
+        .then((res) => {
+          ElMessage.success(res.message)
+          emit("success",)
+          dialogVisible.value = false
+        })
+        .catch((error) => {
+          ElMessage.error("提交失败，请稍后重试");
+        })
+      formLoading.value = false
+    }
+  })
+}
+
+const resetForm = () => {
+  treeNodeAll.value = false
+  menuExpand.value = false
+  treeRef.value?.setCheckedKeys([])
+
+  form.value = {
+    roleId: undefined,
+    roleName: '',
+    roleType: '',
+    menuIds: [],
+  }
+  formRef.value?.resetFields()
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
