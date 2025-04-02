b516a15f6678695be2beb55cc995c3f48373cbb9
Wangxt <2601183227@qq.com>
2025-04-02 22:29:08 +0800

    H

---
+++ sys-manage/src/main/java/com/haohaodayouxi/manage/constants/FilePathConstants.java
@@ -0,0 +1,37 @@
+package com.haohaodayouxi.manage.constants;
+
+import com.haohaodayouxi.common.util.constants.StringConstant;
+
+/**
+ * 文件路径常量
+ */
+public interface FilePathConstants {
+
+    /**
+     * 临时目录
+     */
+    String TMP_FOLDER_PATH = "tmp";
+
+    /**
+     * 临时目录
+     */
+    static String getTmpPath(String objCode) {
+        return TMP_FOLDER_PATH + StringConstant.SLASH + objCode;
+    }
+
+    /**
+     * 公共资源基础路径
+     */
+    String COM_RES_FILES_PATH = "common_res";
+
+    /**
+     * 业务基础路径
+     */
+    String BUS_FILES_PATH = "bus/" + SysConstants.PROJECT_NAME;
+
+    /**
+     * 普通上传基础路径
+     */
+    String UPLOAD_FILES_PATH = BUS_FILES_PATH + "/upload";
+
+}
