35098150d93f24e5be3cd4ba36d747815425dbab
Wangxt <2601183227@qq.com>
2025-04-04 23:36:46 +0800

    H

---
+++ sys-manage/src/main/java/com/haohaodayouxi/manage/service/FileService.java
@@ -0,0 +1,10 @@
+package com.haohaodayouxi.manage.service;
+
+/**
+ * FileService
+ *
+ * @author TONE
+ * @date 2025/4/4
+ */
+public interface FileService {
+}
---
+++ sys-manage/src/main/java/com/haohaodayouxi/manage/service/impl/FileServiceImpl.java
@@ -0,0 +1,17 @@
+package com.haohaodayouxi.manage.service.impl;
+
+import com.haohaodayouxi.manage.service.FileService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * FileServiceImpl
+ *
+ * @author TONE
+ * @date 2025/4/4
+ */
+@Slf4j
+@Service
+public class FileServiceImpl implements FileService {
+    
+}
