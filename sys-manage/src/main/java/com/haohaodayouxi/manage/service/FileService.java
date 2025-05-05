330d3ef95b0b855bb7e61c27fa0773c40d5ec19e
Wangxt <2601183227@qq.com>
2025-05-05 17:06:21 +0800

    H

--- sys-manage/src/main/java/com/haohaodayouxi/manage/service/FileService.java
+++ sys-manage/src/main/java/com/haohaodayouxi/manage/service/FileService.java
@@ -3,7 +3,8 @@ package com.haohaodayouxi.manage.service;
 import com.haohaodayouxi.manage.constants.enums.file.FileObjTypeEnum;
 import com.haohaodayouxi.manage.constants.enums.file.FileTypeEnum;
 import com.haohaodayouxi.manage.model.bo.file.FileInfoBO;
-import org.springframework.web.multipart.MultipartFile;
+import com.haohaodayouxi.manage.model.req.file.FileUploadReq;
+import jakarta.validation.Valid;
 
 import java.util.Map;
 
@@ -18,17 +19,10 @@ public interface FileService {
     /**
      * 文件上传
      *
-     * @param userId       用户id
-     * @param file         文件数据
-     * @param fileTypeEnum 文件类型
-     * @return 预览地址
+     * @param req req
+     * @return FileRes
      */
-    String uploadFile(FileObjTypeEnum objTypeEnum,
-                      Long objId,
-                      Long userId,
-                      MultipartFile file,
-                      FileTypeEnum fileTypeEnum);
-
+    String uploadFile(@Valid FileUploadReq req);
 
     /**
      * 基础 文件上传 不走前置和后置处理
@@ -55,4 +49,40 @@ public interface FileService {
                               String serviceFileName,
                               String encryptKey,
                               Map<String, String> userMetadata);
+
+    default FileInfoBO baseUploadFile(FileObjTypeEnum objTypeEnum,
+                                      Long objId,
+                                      Long userId,
+                                      FileTypeEnum typeEnum,
+                                      String fileName,
+                                      String fileIdeaDir,
+                                      String fileFormat,
+                                      byte[] bytes) {
+        return baseUploadFile(objTypeEnum, objId, userId, typeEnum, fileName, fileIdeaDir, fileFormat, bytes, null, null, null);
+    }
+
+    default FileInfoBO baseUploadFile(FileObjTypeEnum objTypeEnum,
+                                      Long objId,
+                                      Long userId,
+                                      FileTypeEnum typeEnum,
+                                      String fileName,
+                                      String fileIdeaDir,
+                                      String fileFormat,
+                                      byte[] bytes,
+                                      String encryptKey,
+                                      Map<String, String> userMetadata) {
+        return baseUploadFile(objTypeEnum, objId, userId, typeEnum, fileName, fileIdeaDir, fileFormat, bytes, null, encryptKey, userMetadata);
+    }
+
+    default FileInfoBO baseUploadFile(FileObjTypeEnum objTypeEnum,
+                                      Long objId,
+                                      Long userId,
+                                      FileTypeEnum typeEnum,
+                                      String fileName,
+                                      String fileIdeaDir,
+                                      String fileFormat,
+                                      byte[] bytes,
+                                      String encryptKey) {
+        return baseUploadFile(objTypeEnum, objId, userId, typeEnum, fileName, fileIdeaDir, fileFormat, bytes, null, encryptKey, null);
+    }
 }
--- sys-manage/src/main/java/com/haohaodayouxi/manage/service/impl/FileServiceImpl.java
+++ sys-manage/src/main/java/com/haohaodayouxi/manage/service/impl/FileServiceImpl.java
@@ -1,9 +1,41 @@
 package com.haohaodayouxi.manage.service.impl;
 
+import com.alibaba.fastjson2.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.haohaodayouxi.common.core.exception.BusinessException;
+import com.haohaodayouxi.common.util.algorithm.aes.AesUtil;
+import com.haohaodayouxi.common.util.enums.TrueFalseEnum;
+import com.haohaodayouxi.file.core.service.FileUploadCoreService;
+import com.haohaodayouxi.manage.config.param.HParameter;
+import com.haohaodayouxi.manage.constants.FilePathConstants;
+import com.haohaodayouxi.manage.constants.enums.file.FileObjTypeEnum;
+import com.haohaodayouxi.manage.constants.enums.file.FileTypeEnum;
+import com.haohaodayouxi.manage.constants.enums.file.UploadStatusEnum;
+import com.haohaodayouxi.manage.model.bo.file.FileDataCheckBO;
+import com.haohaodayouxi.manage.model.bo.file.FileEncryptBO;
+import com.haohaodayouxi.manage.model.bo.file.FileInfoBO;
+import com.haohaodayouxi.manage.model.bo.file.FileUtilBO;
+import com.haohaodayouxi.manage.model.db.FileUploadLog;
+import com.haohaodayouxi.manage.model.req.file.FileUploadBaseReq;
+import com.haohaodayouxi.manage.model.req.file.FileUploadReq;
+import com.haohaodayouxi.manage.service.FileDataCheckService;
+import com.haohaodayouxi.manage.service.FileOsConfigService;
 import com.haohaodayouxi.manage.service.FileService;
+import com.haohaodayouxi.manage.service.FileUploadLogService;
+import com.haohaodayouxi.manage.utils.file.FfmpegUtil;
+import com.haohaodayouxi.manage.utils.file.FilePathUtil;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * FileServiceImpl
@@ -14,10 +46,219 @@ import org.springframework.web.multipart.MultipartFile;
 @Slf4j
 @Service
 public class FileServiceImpl implements FileService {
+    @Resource
+    private FileUploadLogService fileUploadLogService;
+    @Resource
+    private FileOsConfigService fileOsConfigService;
+    @Resource
+    private FileDataCheckService fileDataCheckService;
+    @Resource
+    private HParameter hParameter;
+    @Resource
+    private FileUploadCoreService fileUploadCoreService;
+
+    private static final String ENCRYPT_KEY = "encryptKey";
 
     @Override
-    public String uploadFile(Long userId, MultipartFile file) {
-        
+    public String uploadFile(@Valid FileUploadReq req) {
+        checkUploadFileReq(req);
+        try {
+            FileInfoBO bo = baseUploadFile(req.getObjTypeEnum(), req.getObjId(), req.getUserId(), req.getTypeEnum(), req.getFile().getOriginalFilename(), FilePathConstants.UPLOAD_BASE_FILES_PATH, req.getFileFormat(), req.getFile().getBytes(), req.getEncryptKey(), req.getUserMetadata());
+            // todo bo转换成预览地址
+        } catch (IOException e) {
+            throw new BusinessException("文件上传异常", e);
+        }
         return "";
+    }
+
+    /**
+     * 校验数据
+     */
+    private void checkUploadFileReq(FileUploadReq req) {
+        checkBaseReq(req);
+        // 文件校验
+        if (ObjectUtils.isEmpty(req.getFile()) || req.getFile().getSize() <= 0 || !Objects.requireNonNull(req.getFile().getOriginalFilename()).contains(".")) {
+            throw new BusinessException("请提供正确的文件");
+        }
+        String fileFormat = FilePathUtil.getFileFormat(req.getFile().getOriginalFilename());
+        FileTypeEnum fileFormatType = FileTypeEnum.getFileType(req.getTypeEnum(), fileFormat);
+        req.setFileFormat(fileFormat);
+        req.setTypeEnum(fileFormatType);
+    }
+
+    /**
+     * 基础校验
+     *
+     * @param req
+     */
+    public void checkBaseReq(FileUploadBaseReq req) {
+        if (ObjectUtils.anyNull(req.getObjTypeEnum(), req.getObjId())) {
+            throw new IllegalArgumentException("对象信息不能为空");
+        }
+    }
+
+    @Override
+    public FileInfoBO baseUploadFile(FileObjTypeEnum objTypeEnum,
+                                     Long objId,
+                                     Long userId,
+                                     FileTypeEnum typeEnum,
+                                     String fileName,
+                                     String fileIdeaDir,
+                                     String fileFormat,
+                                     byte[] bytes,
+                                     String serviceFileName,
+                                     String encryptKey,
+                                     Map<String, String> userMetadata) {
+        FileUtilBO utilBO = fileOsConfigService.getFileUtil(objTypeEnum, objId);
+        if (ObjectUtils.isEmpty(utilBO)) {
+            throw new BusinessException("当前用户不能上传文件");
+        }
+        FileDataCheckBO checkBO = fileDataCheck(typeEnum, bytes);
+        Date now = new Date();
+        Long fileSize = (long) bytes.length;
+        if (ObjectUtils.isEmpty(fileIdeaDir)) {
+            fileIdeaDir = FilePathConstants.UPLOAD_BASE_FILES_PATH;
+        }
+        FileUploadLog uploadLog;
+        if (ObjectUtils.isEmpty(serviceFileName)) {
+            serviceFileName = FilePathUtil.generateFilePath(fileIdeaDir, now, fileFormat);
+            uploadLog = addFileUploadLog(utilBO.getOsId(), fileName, encryptKey, fileSize, serviceFileName, now, objTypeEnum, objId, userId);
+        } else {
+            serviceFileName = fileIdeaDir + serviceFileName;
+            uploadLog = fileUploadLogService.getOne(new LambdaQueryWrapper<FileUploadLog>().eq(FileUploadLog::getServiceFileName, serviceFileName).eq(FileUploadLog::getDelStatus, TrueFalseEnum.FALSE.getCode()).last(" limit 1"));
+            if (ObjectUtils.isEmpty(uploadLog)) {
+                uploadLog = addFileUploadLog(utilBO.getOsId(), fileName, encryptKey, fileSize, serviceFileName, now, objTypeEnum, objId, userId);
+            } else {
+                uploadLog = updFileUploadLog(uploadLog.getFileId(), utilBO.getOsId(), fileName, encryptKey, fileSize, serviceFileName, now, objTypeEnum, objId, userId);
+            }
+        }
+        try {
+            if (ObjectUtils.isEmpty(userMetadata)) {
+                userMetadata = new HashMap<>();
+            }
+            if (ObjectUtils.isNotEmpty(encryptKey)) {
+                bytes = AesUtil.encryptECB(bytes, Base64.decodeBase64(encryptKey));
+                userMetadata.put(ENCRYPT_KEY, encryptKey);
+            }
+            fileUploadCoreService.uploadFile(utilBO.getFileUtilService(), utilBO.getBucketName(), serviceFileName, bytes, userMetadata);
+            FileInfoBO res = FileInfoBO.builder()
+                    .prefixUrl(utilBO.getDomain())
+                    .fileName(fileName)
+                    .fileSize(fileSize)
+                    .serviceFileName(serviceFileName.replace(fileIdeaDir, ""))
+                    .build();
+            if (ObjectUtils.isNotEmpty(checkBO.getZoomData())) {
+                uploadLog.setFileZoomImg(res.getZoomFile());
+                res.setZoomFile(zoomFile(utilBO, typeEnum, now, checkBO.getZoomData()));
+                res.setZoomFileSize((long) checkBO.getZoomData().length);
+            }
+            uploadLog.setUploadStatus(UploadStatusEnum.SUCCESS.getCode());
+            return res;
+        } catch (Exception e) {
+            log.error("基础 文件上传异常 typeEnum:{} fileName:{} fileFormat:{}", typeEnum, fileName, fileFormat);
+            uploadLog.setUploadStatus(UploadStatusEnum.FAIL.getCode());
+            uploadLog.setErrorMsg("文件上传异常 typeEnum:{" + typeEnum + "}" + " fileName:{" + fileName + "}" + " fileFormat:{" + fileFormat + "}" + e);
+            throw new BusinessException("文件上传异常", e);
+        } finally {
+            fileUploadLogService.updateById(uploadLog);
+        }
+    }
+
+    /**
+     * 缩放文件操作
+     *
+     * @param utilBO
+     * @param typeEnum
+     * @param now
+     * @param bytes
+     */
+    private String zoomFile(FileUtilBO utilBO, FileTypeEnum typeEnum, Date now, byte[] bytes) {
+        String zoomFilePath = null;
+        if (ObjectUtils.isNotEmpty(bytes)) {
+            if (typeEnum == FileTypeEnum.VIDEO) {
+                zoomFilePath = FilePathUtil.generateFilePath(FilePathConstants.UPLOAD_BASE_FILES_PATH, now, hParameter.getFileOS().getVideoCoverFormat());
+                fileUploadCoreService.uploadFile(utilBO.getFileUtilService(), utilBO.getBucketName(), zoomFilePath, bytes);
+                zoomFilePath = zoomFilePath.replace(FilePathConstants.UPLOAD_BASE_FILES_PATH, "");
+            }
+        }
+        return zoomFilePath;
+    }
+
+    /**
+     * 添加文件上传日志
+     */
+    private FileUploadLog addFileUploadLog(Long osId,
+                                           String fileName,
+                                           String encryptKey,
+                                           Long fileSize,
+                                           String filePath,
+                                           Date now,
+                                           FileObjTypeEnum objTypeEnum,
+                                           Long objId,
+                                           Long userId) {
+        FileUploadLog log = FileUploadLog.builder()
+                .osId(osId)
+                .fileName(fileName)
+                .encryptInfo(JSON.toJSONString(FileEncryptBO.builder().encryptKey(encryptKey).build()))
+                .fileSize(fileSize)
+                .serviceFileName(FilePathUtil.getFileNameByFilePath(filePath))
+                .filePath(filePath)
+                .objType(objTypeEnum.getType())
+                .objId(objId)
+                .uploadStatus(UploadStatusEnum.UPLOADING.getCode())
+                .createUid(userId)
+                .updateUid(userId)
+                .createTime(now)
+                .updateTime(now)
+                .version(1)
+                .delStatus(TrueFalseEnum.FALSE.getCode())
+                .build();
+        fileUploadLogService.save(log);
+        return log;
+    }
+
+    /**
+     * 更新文件上传日志
+     * 先删除旧数据，再添加新数据
+     */
+    private FileUploadLog updFileUploadLog(Long id,
+                                           Long osId,
+                                           String fileName,
+                                           String encryptKey,
+                                           Long fileSize,
+                                           String filePath,
+                                           Date now,
+                                           FileObjTypeEnum objTypeEnum,
+                                           Long objId,
+                                           Long userId) {
+        FileUploadLog log = FileUploadLog.builder()
+                .fileId(id)
+                .updateUid(userId)
+                .updateTime(now)
+                .delStatus(TrueFalseEnum.TRUE.getCode())
+                .build();
+        fileUploadLogService.updateById(log);
+        return addFileUploadLog(osId, fileName, encryptKey, fileSize, filePath, now, objTypeEnum, objId, userId);
+    }
+
+    /**
+     * 文件数据检测
+     *
+     * @param typeEnum
+     * @param bytes
+     */
+    private FileDataCheckBO fileDataCheck(FileTypeEnum typeEnum, byte[] bytes) {
+        byte[] zoomData = null;
+        boolean nsfw = false;
+        if (typeEnum.equals(FileTypeEnum.IMAGE)) {
+            nsfw = fileDataCheckService.checkNsfw(bytes);
+        } else if (typeEnum.equals(FileTypeEnum.VIDEO)) {
+            zoomData = FfmpegUtil.getVideoCoverImg(bytes, hParameter.getFileOS().getVideoCoverFormat(), hParameter.getFileOS().getVideoCoverWith());
+            nsfw = fileDataCheckService.checkNsfw(zoomData);
+        }
+        if (nsfw) {
+            throw new BusinessException("文件含有违规内容，不能上传");
+        }
+        return FileDataCheckBO.builder().data(bytes).zoomData(zoomData).build();
     }
 }
