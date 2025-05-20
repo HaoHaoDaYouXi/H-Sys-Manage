53106878ef8d46f9812171e8b0d5a9e1d415d9db
Wangxt <2601183227@qq.com>
2025-05-20 23:03:20 +0800

    H

--- sys-manage-vue/src/api/sys/menu/types/menu.ts
+++ sys-manage-vue/src/api/sys/menu/types/menu.ts
@@ -1,71 +1,71 @@
 export interface SMenuListReq {
-  menuParentId: number,
-  menuType?: number,
-  menuName: string,
-  disabled?: number,
+  menuParentId: number
+  menuType?: string
+  menuName: string
+  disabled?: number
 }
 
 export interface SMenuList {
-  menuId: number,
-  menuParentId: number,
-  menuType: number,
-  menuTypeStr: string,
-  menuName: string,
-  menuIcon: string,
-  menuKey: string,
-  menuComponent: string,
-  showOrder: number,
-  disabled: number,
-  updateTime: string,
+  menuId: number
+  menuParentId: number
+  menuType: string
+  menuTypeStr: string
+  menuName: string
+  menuIcon: string
+  menuKey: string
+  menuComponent: string
+  showOrder: number
+  disabled: number
+  updateTime: string
   hasChildren: boolean
 }
 
 export type SMenuListRes = ApiRes<SMenuList[]>
 
 export interface SMenuAddOrUpdReq {
-  menuId: number,
-  menuParentId: number,
-  menuType: number,
-  menuName: string,
-  menuIcon: string,
-  menuKey: string,
-  activeMenu: string,
-  redirect: string,
-  menuComponent: string,
-  outUrl: number,
-  showOrder: number,
-  disabled: number,
-  hidden: number,
-  cachedView: number,
-  breadcrumb: number,
-  affix: number,
-  alwaysShow: number,
+  menuId: number
+  menuParentId: number
+  menuType: string
+  menuName: string
+  menuIcon: string
+  menuKey: string
+  activeMenu: string
+  redirect: string
+  menuComponent: string
+  outUrl: number
+  showOrder: number
+  disabled: number
+  hidden: number
+  cachedView: number
+  breadcrumb: number
+  affix: number
+  alwaysShow: number
   menuDescribe: string
 }
 
 export interface SMenuDeTail {
-  "menuId": number,
-  "menuParentId": number,
-  "menuType": number,
-  "menuName": string,
-  "menuIcon": string,
-  "menuKey": string,
-  "activeMenu": string,
-  "redirect": string,
-  "menuComponent": string,
-  "outUrl": number,
-  "showOrder": number,
-  "disabled": number,
-  "hidden": number,
-  "cachedView": number,
-  "breadcrumb": number,
-  "affix": number,
-  "alwaysShow": number,
-  "menuDescribe": string,
-  "createUid": number,
-  "updateUid": number,
-  "createTime": string,
-  "updateTime": string,
-  "version": number,
-  "delStatus": number
+  menuId: number
+  menuParentId: number
+  menuType: string
+  menuName: string
+  menuIcon: string
+  menuKey: string
+  activeMenu: string
+  redirect: string
+  menuComponent: string
+  outUrl: number
+  showOrder: number
+  disabled: number
+  hidden: number
+  cachedView: number
+  breadcrumb: number
+  affix: number
+  alwaysShow: number
+  menuDescribe: string
+  createUid: number
+  updateUid: number
+  createTime: string
+  updateTime: string
+  version: number
+  delStatus: number
 }
