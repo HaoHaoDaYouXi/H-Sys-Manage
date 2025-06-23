37e5f44258ba3956100a35af1fa3547df5d92212
Wangxt <2601183227@qq.com>
2025-06-23 22:20:29 +0800

    H

--- sys-manage-vue/src/views/sys/param/index.vue
+++ sys-manage-vue/src/views/sys/param/index.vue
@@ -3,23 +3,12 @@
     <el-card v-loading="loading" shadow="never">
       <div class="toolbar-wrapper">
         <div>
-          <el-button
-            type="primary"
-            :icon="CirclePlus"
-            @click="openItem(undefined)"
-          >
-            新增
-          </el-button>
+          <el-button type="primary" :icon="CirclePlus" @click="openItem(undefined)">新增</el-button>
           <el-button type="danger" :icon="Delete" @click="batchDel">批量删除</el-button>
         </div>
         <div>
           <el-tooltip content="刷新当前页">
-            <el-button
-              type="primary"
-              :icon="RefreshRight"
-              circle
-              @click="getTableData"
-            />
+            <el-button type="primary" :icon="RefreshRight" circle @click="getTableData" />
           </el-tooltip>
         </div>
       </div>
@@ -40,31 +29,10 @@
           <el-table-column prop="paramValue" label="参数值" align="center" />
           <el-table-column prop="paramSortCode" label="顺序" align="center" />
           <el-table-column prop="updateTime" label="更新时间" align="center" />
-          <el-table-column
-            fixed="right"
-            label="操作"
-            width="150"
-            align="center"
-          >
+          <el-table-column fixed="right" label="操作" width="150" align="center">
             <template #default="scope">
-              <el-button
-                type="primary"
-                text
-                bg
-                size="small"
-                @click="openItem(scope.row.paramCode)"
-              >
-                修改
-              </el-button>
-              <el-button
-                type="danger"
-                text
-                bg
-                size="small"
-                @click="handleDelete(scope.row)"
-              >
-                删除
-              </el-button>
+              <el-button type="primary" text bg size="small" @click="openItem(scope.row.paramCode)">修改</el-button>
+              <el-button type="danger" text bg size="small" @click="handleDelete(scope.row)">删除</el-button>
             </template>
           </el-table-column>
         </el-table>
@@ -77,9 +45,9 @@
 <script lang="ts" setup>
 import { ref, onMounted } from "vue"
 import { ElMessage, ElMessageBox, ElTable } from "element-plus"
-import {CirclePlus, Delete, Download, RefreshRight} from "@element-plus/icons-vue"
+import { CirclePlus, Delete, Download, RefreshRight } from "@element-plus/icons-vue"
 import { batchDelApi } from "@/api/sys/param"
-import { TopId, } from "@/utils/enums"
+import { TopId } from "@/utils/enums"
 import Item from "./item.vue"
 import { ListObjectBO } from "@/api/commonTypes"
 import { getAllParamApi } from "@/api/sys/param"
@@ -96,14 +64,15 @@ const tableData = ref<any[]>([])
 const getTableData = async () => {
   try {
     const { data } = await getAllParamApi()
-    tableData.value = getChild(data,TopId)
+    tableData.value = getChild(data, TopId)
   } catch (error) {
     ElMessage.error("获取列表失败")
   }
 }
 
 const getChild = (data: any[], parentId: any): any[] => {
-  const child = data.filter((item) => item.paramParentCode === parentId)
+  const child = data
+    .filter((item) => item.paramParentCode === parentId)
     .sort((a, b) => {
       if (a.paramSortCode < b.paramSortCode) return -1
       if (a.paramSortCode > b.paramSortCode) return 1
@@ -120,7 +89,7 @@ const getChild = (data: any[], parentId: any): any[] => {
         children: getChild(data, item.paramCode)
       }
     })
-  }else {
+  } else {
     return []
   }
 }
@@ -140,12 +109,14 @@ const handleDelete = (row: any) => {
     confirmButtonText: "确定",
     cancelButtonText: "取消"
   }).then(async () => {
-    batchDelApi({ list: [row.paramCode]} as ListObjectBO).then((res) => {
-      ElMessage.success("删除成功")
-      itemSuccess()
-    }).catch((error) => {
-      ElMessage.error("删除失败")
-    })
+    batchDelApi({ list: [row.paramCode] } as ListObjectBO)
+      .then((res) => {
+        ElMessage.success("删除成功")
+        itemSuccess()
+      })
+      .catch((error) => {
+        ElMessage.error("删除失败")
+      })
   })
 }
 
@@ -155,17 +126,19 @@ const batchDel = async () => {
     confirmButtonText: "确定",
     cancelButtonText: "取消"
   }).then(async () => {
-    batchDelApi({ list: tableDataRef.value?.getSelectionRows()?.map((m: any) => m.paramCode) } as ListObjectBO).then((res) => {
-      ElMessage.success("删除成功")
-      itemSuccess()
-    }).catch((error) => {
-      ElMessage.error("删除失败")
-    })
+    batchDelApi({ list: tableDataRef.value?.getSelectionRows()?.map((m: any) => m.paramCode) } as ListObjectBO)
+      .then((res) => {
+        ElMessage.success("删除成功")
+        itemSuccess()
+      })
+      .catch((error) => {
+        ElMessage.error("删除失败")
+      })
   })
 }
 
 const init = async () => {
-  if (loading.value) return;
+  if (loading.value) return
   loading.value = true
   await getTableData()
   loading.value = false
@@ -174,9 +147,6 @@ const init = async () => {
 onMounted(async () => {
   await init()
 })
-
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
