af5d6c8a1b4c3b2b346f0785612df12eea2c4d0c
Wangxt <2601183227@qq.com>
2025-06-29 21:56:07 +0800

    H

--- sys-manage/sys/sys-service-api/src/main/java/com/haohaodayouxi/manage/model/req/param/SParamReq.java
+++ sys-manage/sys/sys-service-api/src/main/java/com/haohaodayouxi/manage/model/req/param/SParamReq.java
@@ -35,4 +35,11 @@ public class SParamReq implements Serializable {
      * 参数分类(上级编码，0-第一级)
      */
     private Long paramParentCode;
+
+    /**
+     * 需要设置paramParentCode，并且无paramCode和paramCodes才有用
+     * 子集深度 null|0-不限制 1-一级子集 2-二级子集 3-三级子集
+     * 根据深度 控制子集ID查询长度
+     */
+    private Integer childDeep;
 }
