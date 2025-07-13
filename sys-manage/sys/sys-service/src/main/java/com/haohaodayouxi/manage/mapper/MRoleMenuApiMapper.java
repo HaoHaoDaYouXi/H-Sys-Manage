9a97b24abf66607d4b1453cb203b10da05fec65c
Wangxt <2601183227@qq.com>
2025-07-13 21:54:21 +0800

    H

---
+++ sys-manage/sys/sys-service/src/main/java/com/haohaodayouxi/manage/mapper/MRoleMenuApiMapper.java
@@ -0,0 +1,13 @@
+package com.haohaodayouxi.manage.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.haohaodayouxi.manage.model.db.MRoleMenuApi;
+
+/**
+ * MRoleMenuApiMapper
+ *
+ * @author TONE
+ * @date 2025/7/13
+ */
+public interface MRoleMenuApiMapper extends BaseMapper<MRoleMenuApi> {
+}
---
+++ sys-manage/sys/sys-service/src/main/java/com/haohaodayouxi/manage/model/db/MRoleMenuApi.java
@@ -0,0 +1,84 @@
+package com.haohaodayouxi.manage.model.db;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * MRoleMenuApi
+ *
+ * @author TONE
+ * @date 2025/7/13
+ */
+/**
+ * 角色菜单关联表
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName(value = "m_role_menu_api")
+public class MRoleMenuApi implements Serializable {
+    /**
+     * 主键ID
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 角色ID
+     */
+    @TableField(value = "role_id")
+    private Long roleId;
+
+    /**
+     * 菜单接口关联ID(m_menu_api)
+     */
+    @TableField(value = "menu_api_id")
+    private Long menuApiId;
+
+    /**
+     * 创建人
+     */
+    @TableField(value = "create_uid")
+    private Long createUid;
+
+    /**
+     * 修改人
+     */
+    @TableField(value = "update_uid")
+    private Long updateUid;
+
+    /**
+     * 创建时间
+     */
+    @TableField(value = "create_time")
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField(value = "update_time")
+    private Date updateTime;
+
+    /**
+     * 版本
+     */
+    @TableField(value = "version")
+    private Long version;
+
+    /**
+     * 删除状态（0，正常，1已删除）
+     */
+    @TableField(value = "del_status")
+    private Integer delStatus;
+
+    private static final long serialVersionUID = 1L;
+}
