8affeca3ce04d3205221079e6c18914160a46f3d
Wangxt <2601183227@qq.com>
2025-07-19 14:54:01 +0800

    H

---
+++ sys-manage-vue/src/api/sys/api/index.ts
@@ -0,0 +1,19 @@
+import { request } from "@/utils/service"
+import { commonApi } from "@/api/commonApi"
+
+const API_PREFIX = "s_api"
+const baseApi = commonApi(API_PREFIX)
+export const pageListApi = baseApi.pageListApi
+export const addApi = baseApi.addApi
+export const detailApi = baseApi.detailApi
+export const updApi = baseApi.updApi
+export const batchDelApi = baseApi.batchDelApi
+
+/** 获取模块列表 */
+export function getModuleList(data: any) {
+  return request<ApiRes<any>>({
+    url: `${API_PREFIX}/getModuleList`,
+    method: "post",
+    data
+  })
+}
