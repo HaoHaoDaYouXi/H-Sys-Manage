a56147db3085bcfda4f90cc6cfb05b8618eaafef
Wangxt <2601183227@qq.com>
2025-07-19 14:53:50 +0800

    H

--- sys-manage-vue/src/api/commonApi.ts
+++ sys-manage-vue/src/api/commonApi.ts
@@ -4,6 +4,13 @@ import { ListObjectBO } from "@/api/commonTypes"
 
 export function commonApi<Prefix extends string>(API_PREFIX: Prefix) {
   return {
+    pageListApi: <T = boolean>(data: any) =>
+      request<ApiRes<T>>({
+        url: `${API_PREFIX}/pageList`,
+        method: "post",
+        data
+      }),
+
     addApi: <T = boolean>(data: any) =>
       request<ApiRes<T>>({
         url: `${API_PREFIX}/add`,
