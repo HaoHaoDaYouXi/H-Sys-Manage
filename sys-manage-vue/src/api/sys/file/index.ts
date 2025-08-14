061c4c55b161aeb8d0b7ab8015a49313f097996e
Wangxt <2601183227@qq.com>
2025-08-14 23:10:36 +0800

    H

--- sys-manage-vue/src/api/sys/file/index.ts
+++ sys-manage-vue/src/api/sys/file/index.ts
@@ -8,3 +8,18 @@ export const addApi = baseApi.addApi
 export const detailApi = baseApi.detailApi
 export const updApi = baseApi.updApi
 export const batchDelApi = baseApi.batchDelApi
+
+/** 根据编码获取文件预览地址 */
+export function getPreviewUrlApi(fileCode: any) {
+  return request<ApiRes<any>>({
+    url: `${API_PREFIX}/getPreviewUrl/${fileCode}`,
+    method: "get"
+  })
+}
+/** 预览文件 */
+export function previewApi(fileName: any) {
+  return request<ApiRes<any>>({
+    url: `${API_PREFIX}/preview/${fileName}`,
+    method: "get"
+  })
+}
