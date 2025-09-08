fd379067a91f9f82c43b123cfe362061ce20584e
Wangxt <2601183227@qq.com>
2025-09-08 22:09:28 +0800

    H

--- sys-manage-vue/src/components/upload/index.vue
+++ sys-manage-vue/src/components/upload/index.vue
@@ -1,14 +1,167 @@
 <template>
   <div class="upload-wrapper">
-    <component :is="h(ElUpload, { ...$attrs, ...props, ref: changeRef }, $slots)" />
+    <el-upload
+      ref="uploadRef"
+      name="file"
+      :class="[
+        props.fileList.length >= props.limit ? 'hide' : props.fileType !== 3 ? 'flex flex-wrap' : '',
+        !uploadParams.api ? 'is-disabled' : ''
+      ]"
+      :action="uploadParams.api || ''"
+      :headers="headers"
+      :data="props.formData"
+      :accept="uploadParams.extension"
+      :limit="props.limit"
+      :file-list="props.fileList"
+      :list-type="props.listType"
+      :multiple="props.limit > 1"
+      :on-preview="handlePreview"
+      :on-remove="handleRemove"
+      :on-success="handleAvatarSuccess"
+      :on-error="handleAvatarError"
+      :on-exceed="handleExceed"
+      :before-upload="beforeAvatarUpload"
+    >
+      <i class="el-icon-plus" v-if="props.listType === 'picture-card'" />
+      <el-button v-else icon="el-icon-paperclip" round>{{ props.placeholder }}</el-button>
+      <template #tip v-if="!!props.tip">
+        <div class="el-upload__tip">{{ computedTip }}</div>
+      </template>
+    </el-upload>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { getCurrentInstance, h } from "vue"
-import { ElUpload, type UploadProps } from "element-plus"
+import { computed, getCurrentInstance, h, ref, withDefaults } from "vue"
+import {
+  ElMessage,
+  ElUpload,
+  UploadFile,
+  UploadFiles,
+  UploadProgressEvent,
+  type UploadProps,
+  UploadRawFile,
+  UploadUserFile
+} from "element-plus"
+import { uploadTypeEnum } from "@/utils/enums"
+import { getToken } from "@/utils/cache/cookies"
+import { getBaseURL } from "@/utils/service"
+import { API_PREFIX } from "@/api/sys/file"
 
-const props = defineProps<Partial<UploadProps>>()
+const props = withDefaults(defineProps<Partial<UploadProps>>(), {
+  fileType: 1, // 文件保存的路径类型 0-其他 1-图片 2-附件 3-图片和附件 4-音频 5-视频 6-音频和视频
+  fileCount: 0,
+  limit: 1,
+  action: "",
+  placeholder: "选择文件",
+  maxSize: 5 * 1024 * 1024, // 文件大小最大默认5M
+  tip: "",
+  width: 0,
+  height: 0,
+  formKey: "",
+  formData: () => {
+    return {}
+  },
+  fileList: () => {
+    return []
+  },
+  listType: "text" // text/picture/picture-card
+})
+
+// 动态计算tip属性
+const computedTip = computed(() => {
+  const m = props.maxSize / 1024 / 1024
+  return props.tip || `只能上传${uploadParams.value.extension}文件，且不超过 ${m > 1024 ? m / 1024 + " G" : m + " M"}`
+})
+
+const uploadRef = ref()
+const uploadParams = computed(() => {
+  switch (props.fileType) {
+    case 2:
+      return { ...uploadTypeEnum.DOC, api: getBaseURL(API_PREFIX) + uploadTypeEnum.DOC.api }
+    case 3:
+      return { ...uploadTypeEnum.IMG_DOC, api: getBaseURL(API_PREFIX) + uploadTypeEnum.IMG_DOC.api }
+    case 4:
+      return { ...uploadTypeEnum.AUDIO, api: getBaseURL(API_PREFIX) + uploadTypeEnum.AUDIO.api }
+    case 5:
+      return { ...uploadTypeEnum.VIDEO, api: getBaseURL(API_PREFIX) + uploadTypeEnum.VIDEO.api }
+    default:
+      return { ...uploadTypeEnum.IMG, api: getBaseURL(API_PREFIX) + uploadTypeEnum.IMG.api }
+  }
+})
+
+const headers = ref({
+  "H-Token": getToken()
+})
+
+const emit = defineEmits(["changeFile"])
+
+const handlePreview = (uploadFile: UploadFile) => {
+  console.log("handlePreview")
+}
+const handleRemove = (uploadFile: any) => {
+  let serviceFileName = uploadFile.url
+  if (uploadFile.response && uploadFile.response?.data) {
+    serviceFileName = uploadFile.response.data?.serviceFileName
+  }
+  emit("changeFile", { type: "remove", formKey: props.formKey, fileName: uploadFile.name, serviceFileName })
+}
+const handleAvatarSuccess = (res: any, uploadFile: any) => {
+  if (res.code === 20000) {
+    const temp = res.data || {}
+    emit("changeFile", { type: "add", formKey: props.formKey, ...temp })
+    if (temp.zoomFile) {
+      uploadFile.url = `${temp.prefixUrl}${temp.zoomFile}`
+    }
+  } else {
+    ElMessage.error(res.message)
+    handleRemove("")
+  }
+}
+const handleAvatarError = (error: Error, uploadFile: UploadFile, uploadFiles: UploadFiles) => {
+  console.log("handleAvatarError", error)
+}
+const handleExceed = (files: File[], uploadFiles: UploadUserFile[]) => {
+  ElMessage.error(`最大上传文件数量为 ${props.limit}`)
+}
+const beforeAvatarUpload = (rawFile: UploadRawFile) => {
+  const format = rawFile.name.substring(rawFile.name.lastIndexOf("."))
+  if (!uploadParams.value.extension.split(",").concat(format)) {
+    ElMessage.error(`只能上传 ${uploadParams.value.extension || ""} 文件`)
+    return false
+  }
+  if (rawFile.size > props.maxSize) {
+    const m = props.maxSize / 1024 / 1024
+    ElMessage.error(`文件大小不能超过 ${m > 1024 ? m / 1024 + " G" : m + " M"}`)
+    return false
+  }
+  if (props.width && props.height) {
+    return new Promise<void>(function (resolve, reject) {
+      const _URL = window.URL || window.webkitURL
+      const img = new Image()
+      const objectUrl = _URL.createObjectURL(rawFile)
+      img.onload = function () {
+        const valid = img.width <= props.width && img.height <= props.height
+        _URL.revokeObjectURL(objectUrl)
+        valid ? resolve() : reject()
+      }
+      img.onerror = function () {
+        _URL.revokeObjectURL(objectUrl)
+        reject(new Error("图片加载失败"))
+      }
+      img.src = objectUrl
+    })
+      .then(() => {
+        return true
+      })
+      .catch(() => {
+        ElMessage.error(`上传图片像素最大是${props.width}*${props.height}!`)
+        return false
+      })
+  }
+  return true
+}
+
 const vm = getCurrentInstance()
 function changeRef(instance: any) {
   if (vm) {
